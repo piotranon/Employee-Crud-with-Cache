9da20acb2722df6f9504846beb6d52cc3521d785
Piotr Długosz <piotranon.1@gmail.com>
2021-11-25 21:18:30 +0100

    destroy fix missing entity model

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -148,8 +148,9 @@ class EmployeeController extends Controller
      * @param  \App\Models\Employee  $employee
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Employee $employee)
+    public function destroy($id)
     {
+        $employee = Employee::where('id',$id)->firstOrFail();
         $employee->delete();
 
         Cache::forget('employee-'.$id);
